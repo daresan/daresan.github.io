0b34f102e307c89db0df2db5e44079890051fdc6
Daniel Hinnerkopf <daresan21@gmail.com>
2025-04-25 23:10:25 +0200

    further structure changes

---
+++ example/src/app.mjs
@@ -0,0 +1,13 @@
+export const state = {
+  app: {
+    title: 'Custom App Title',
+    description: 'Custom App Description',
+  },
+
+  menu: [
+    { to: '/', text: 'home' },
+    { to: '/deep/', text: 'deep' },
+  ],
+
+  logo: '/logo.png',
+}
